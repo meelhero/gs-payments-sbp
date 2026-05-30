1c1f90d9b067d0da3e14801a470ee36866fe8b39
meelhero <meelhero@gmail.com>
2026-05-31 02:23:20 +0600

    Update create-payment.js

--- api/create-payment.js
+++ api/create-payment.js
@@ -1,5 +1,24 @@
 export default async function handler(req, res) {
 
+  res.setHeader(
+    'Access-Control-Allow-Origin',
+    'https://grange-see.com'
+  );
+
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS'
+  );
+
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Content-Type'
+  );
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   if (req.method !== 'POST') {
     return res.status(405).json({
       error: 'Method not allowed'
@@ -101,9 +120,11 @@ export default async function handler(req, res) {
       await response.json();
 
     if (!response.ok) {
+
       return res
-        .status(400)
+        .status(response.status)
         .json(data);
+
     }
 
     return res.status(200).json({
